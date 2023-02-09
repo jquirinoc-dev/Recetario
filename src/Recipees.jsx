adf19eb722efc1707476973dbdc029d62e35cb84
Jonathan J Quirino <75098397+jquirinoc-dev@users.noreply.github.com>
2023-02-09 16:10:45 -0600

    Nothing found coimponent

--- src/Recipees.jsx
+++ src/Recipees.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
+import { NothingFound } from './NothingFound'
 import { Recipe } from './Recipe'
 
 export const Recipees = ( { recipees } ) => {
   return (
     <div className="recipees-container">
-        {recipees.map(recipe => (
-            <Recipe
-            key={recipe.id}
-            title={recipe.title}
-            image={recipe.image}
-            desc={recipe.desc}
-            />
-        ))}
+        {recipees.length > 0 ?
+            recipees.map(recipe => (
+                <Recipe
+                key={recipe.id}
+                title={recipe.title}
+                image={recipe.image}
+                desc={recipe.desc}
+                />
+            ))
+            :
+            <NothingFound />
+        }
     </div>
   )
 }
